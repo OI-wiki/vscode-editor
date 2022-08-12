fb6cece03c68c2f18be9845f4d12fa211cd1d490
faga1 <1064827638@qq.com>
2022-08-13 04:55:39 +0800

    fix: 解决rehype-source-line 修改node properties错误问题

--- extensions/remark-renderer/src/web/plugin/rehype-source-line.ts
+++ extensions/remark-renderer/src/web/plugin/rehype-source-line.ts
@@ -1,33 +1,14 @@
 import {visit} from 'unist-util-visit';
 
-interface Options{
-  propertyName?:string,
-  value?:string|number|string[]
-}
-export default function rehypeSourceLine(options?:Options) {
+
+export default function rehypeSourceLine() {
   return (tree:any) => {
     visit(tree, (node) => {
-        if(node.position){
-          if(!options||!options.propertyName&&!options.value){
-            node.properties = {dataLine: node.position.start.line};
-          }else{
-            const propertyName = options.propertyName?options.propertyName:'dataLine';
-            if(options.value){
-              const { value } = options;
-              if(Array.isArray(value)){
-                const realValue = value.reduce((pre,cur)=>{
-                  return pre[cur];
-                },node);
-                node.properties = {[propertyName]:realValue};
-              }else{
-                node.properties = {[propertyName]:value};
-              }
-            }else{
-              node.properties = {[propertyName]:node.position.start.line};
-            }
-          }
-          node.properties.class = 'code-line';
-        }
+      if (node?.position?.start?.line) {
+        if (!node.properties) node.properties = {};
+        node.properties.class = 'code-line';
+        node.properties.dataLine = node.position.start.line;
+      }
     });
   };
 }
