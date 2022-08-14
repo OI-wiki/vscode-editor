81ee061d36d4ac9e3e80bb173a227a7f57187ab3
faga1 <1064827638@qq.com>
2022-08-14 15:12:07 +0800

    feat: delete log

--- extensions/remark-renderer/src/web/preview.ts
+++ extensions/remark-renderer/src/web/preview.ts
@@ -57,25 +57,18 @@ export default class MarkdownPreview {
 	}
 	public async insertSnippets(rawText:string):Promise<string>{
 		const s = new MagicString(rawText);
+		// get rootpath
 		const rootPath = vscode.workspace.workspaceFolders ??[];
-		console.log(rootPath[0]);
-		
+		// match every import file grammar
 		for(const item of rawText.matchAll(/--8<--\s*(.*)/g)){
 			const start = item.index ?? 0;
 			const end = start + item[0].length;
 			const filePath = item[1].slice(1,-1);
-			console.log(filePath,start);
-			// const absolutePath = path.resolve(rootPath[0].uri.path, filePath);
-			// const uri = vscode.Uri.file(absolutePath);
-			// const uri = vscode.Uri.parse();
 			const uri = vscode.Uri.joinPath(rootPath[0].uri,filePath);
-			
-			console.log(uri);
 			const uint8arr = await vscode.workspace.fs.readFile(uri);
+			// transform uint8arr to string
 			const content = String.fromCharCode.apply(null,Array.from(uint8arr));
-			// console.log(String.fromCharCode.apply(null, content));
 			s.overwrite(start,end,content);
-			// s.appendRight(start,content);
 		}
 		return s.toString();
 	}
