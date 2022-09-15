7233a90431948e67730980cd5654753d2062830d
lzc <liuzhaochen.faga@bytedance.com>
2022-09-15 23:28:05 +0800

    feat: throttle register event

--- extensions/remark-renderer/src/web/extension.ts
+++ extensions/remark-renderer/src/web/extension.ts
@@ -7,6 +7,7 @@ const throttle = require('lodash.throttle');
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('remark-renderer.rendererView', () => {
 		const editor = vscode.window.activeTextEditor;
+		let content = editor?.document.getText();
 		if(editor?.document.languageId !== 'markdown') {
 			vscode.window.showErrorMessage("It's not a markdown file");
 			return;
@@ -23,27 +24,29 @@ export function activate(context: vscode.ExtensionContext) {
 		// init preview line
 		panel.webview.postMessage({
 			command: "changeTextEditorSelection",
-			// line:midLine,
 			line: editor["visibleRanges"][0].start.line,
 		});
-		vscode.window.onDidChangeTextEditorSelection(throttle(async (e:vscode.TextEditorSelectionChangeEvent)=>{
+		const textEditorSelectionChange = throttle(async (e:vscode.TextEditorSelectionChangeEvent)=>{
+			if (!e.textEditor.document.fileName.endsWith('.md')) return;
 			const newText = e.textEditor.document.getText();
+			// if (content === newText) return;
+			// content = newText;		
 			panel.webview.html = await markdownPreview.getHtml(newText);
-		}),10000);
-		vscode.window.onDidChangeTextEditorVisibleRanges(throttle((e:vscode.TextEditorVisibleRangesChangeEvent)=>{
-			// const topLine = getTopVisibleLine(e.textEditor);
-			// const bottomLine = getBottomVisibleLine(e.textEditor);
-			// let midLine;
-			// if(topLine&&bottomLine){
-			// 	midLine = Math.floor((topLine + bottomLine) / 2);
-			// }
+		},50);
+		const textEditorVisibleRangesChange = throttle((e:vscode.TextEditorVisibleRangesChangeEvent)=>{
+			
 			if(markdownPreview.isFromWebview) return;
+			console.log('texteditorvis');
+
 			panel.webview.postMessage({
 				command: "changeTextEditorSelection",
 				// line:midLine,
 				line: e.textEditor["visibleRanges"][0].start.line,
 			});
-		}),16);
+		},16)
+		vscode.window.onDidChangeTextEditorSelection(textEditorSelectionChange);
+		
+		vscode.window.onDidChangeTextEditorVisibleRanges(textEditorVisibleRangesChange)
 	});
 
 	context.subscriptions.push(disposable);
