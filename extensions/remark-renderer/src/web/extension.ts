e9d1e5c6cf0b0400c0263a73bba657550d849d15
faga1 <1064827638@qq.com>
2022-09-28 01:05:57 +0800

    refacor: refactor markdown preview class

--- extensions/remark-renderer/src/web/extension.ts
+++ extensions/remark-renderer/src/web/extension.ts
@@ -5,48 +5,7 @@ const throttle = require('lodash.throttle');
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('remark-renderer.rendererView', () => {
-		const editor = vscode.window.activeTextEditor;
-		if(editor?.document.languageId !== 'markdown') {
-			vscode.window.showErrorMessage("It's not a markdown file");
-			return;
-		}
-		const panel = vscode.window.createWebviewPanel(
-			'mdRenderer',
-			'Preview',
-			vscode.ViewColumn.Beside,
-			{
-				enableScripts: true,
-			}
-		);
-		const markdownPreview = new MarkdownPreview(panel,editor,context);
-		// init preview line
-		panel.webview.postMessage({
-			command: "changeTextEditorSelection",
-			line: editor["visibleRanges"][0].start.line,
-		});
-		const textEditorSelectionChange = throttle(async (e:vscode.TextEditorSelectionChangeEvent)=>{
-			if (!e.textEditor.document.fileName.endsWith('.md')) return;
-			const newText = e.textEditor.document.getText();
-			// if (content === newText) return;
-			// content = newText;		
-			const content = await markdownPreview.getHtmlContent(newText);
-			panel.webview.postMessage({
-				command: 'updateContent',
-				content
-			});
-		},50);
-		const textEditorVisibleRangesChange = throttle((e:vscode.TextEditorVisibleRangesChangeEvent)=>{
-			// rangeChange caused by webview
-			if(markdownPreview.isFromWebview) return;
-			panel.webview.postMessage({
-				command: "changeTextEditorSelection",
-				// line:midLine,
-				line: e.textEditor["visibleRanges"][0].start.line,
-			});
-		},16)
-		vscode.window.onDidChangeTextEditorSelection(textEditorSelectionChange);
-		
-		vscode.window.onDidChangeTextEditorVisibleRanges(textEditorVisibleRangesChange)
+		new MarkdownPreview(context);
 	});
 
 	context.subscriptions.push(disposable);
--- extensions/remark-renderer/src/web/preview.ts
+++ extensions/remark-renderer/src/web/preview.ts
@@ -7,31 +7,51 @@ import delayThrottle from '../../utils/delayThrottle';
 import MagicString from 'magic-string';
 
 export default class MarkdownPreview {
-    private readonly _webviewPanel: vscode.WebviewPanel;
-    private readonly _editor: vscode.TextEditor | undefined;
+    private _webviewPanel: vscode.WebviewPanel;
+    private _editor: vscode.TextEditor | undefined = vscode.window.activeTextEditor;
 	private readonly _context: vscode.ExtensionContext;
 	private webRes:WebResource;
-	private renderer:any;
+	private _renderer:any;
 	public isFromWebview: boolean = false;
     constructor(
-        webviewPanel: vscode.WebviewPanel,
-        editor: vscode.TextEditor | undefined,
 		context: vscode.ExtensionContext
     ){
-        this._webviewPanel = webviewPanel;
-		this.renderer = getPipeline(this._webviewPanel);
-        this._editor = editor;
+		this._webviewPanel = this.createPanel();
+		this._renderer = getPipeline(this._webviewPanel);
 		this._context = context;
 		this.initWebviewHtml();
 		this.handleMessage();
 		this.webRes = new WebResource(this._webviewPanel.webview, this._context.extensionUri);
+		if(this._editor?.document.languageId !== 'markdown') {
+			vscode.window.showErrorMessage("It's not a markdown file");
+			return;
+		}
+		// init preview line
+		this._webviewPanel.webview.postMessage({
+			command: "changeTextEditorSelection",
+			line: this._editor["visibleRanges"][0].start.line,
+		});
+		this.registerEvent();
 	}
+	private  createPanel() {
+		return vscode.window.createWebviewPanel(
+			'mdRenderer',
+			'Preview',
+			vscode.ViewColumn.Beside,
+			{
+				enableScripts: true,
+			}
+		);
+	}
+
 	private async initWebviewHtml(){
 		this._webviewPanel.webview.html = await this.getHtml(this._editor?.document.getText()?? "*No preview available*");
 	}
+
 	private setIsFromWebView = delayThrottle(()=>{
 		this.isFromWebview = false;
 	},100);
+
     public handleMessage(){
         this._webviewPanel.webview.onDidReceiveMessage(
 			(message) => {
@@ -53,16 +73,44 @@ export default class MarkdownPreview {
 			}
 		);
     }
+
+	public registerEvent(){
+		const textEditorSelectionChange = throttle(async (e:vscode.TextEditorSelectionChangeEvent)=>{
+			if (!e.textEditor.document.fileName.endsWith('.md')) return;
+			const newText = e.textEditor.document.getText();
+			// if (content === newText) return;
+			// content = newText;		
+			const content = await this.getHtmlContent(newText);
+			this._webviewPanel.webview.postMessage({
+				command: 'updateContent',
+				content
+			});
+		},16);
+		const textEditorVisibleRangesChange = throttle((e:vscode.TextEditorVisibleRangesChangeEvent)=>{
+			// rangeChange caused by webview
+			if(this.isFromWebview) return;
+			this._webviewPanel.webview.postMessage({
+				command: "changeTextEditorSelection",
+				// line:midLine,
+				line: e.textEditor["visibleRanges"][0].start.line,
+			});
+		},16)
+		vscode.window.onDidChangeTextEditorSelection(textEditorSelectionChange);
+		
+		vscode.window.onDidChangeTextEditorVisibleRanges(textEditorVisibleRangesChange)
+	}
 	public async getHtml(md: string) : Promise<string> {
 		const htmlContent = await this.getHtmlContent(md);
 		return this.webRes.genRenderHtml(htmlContent);
 	}
+
 	// get html body content
 	public async getHtmlContent(md:string): Promise<string>{
-		const html = this.renderer.processSync(md).toString();
+		const html = this._renderer.processSync(md).toString();
 		const content =  await this.insertSnippets(html);
 		return this.webRes.getBody(content);
 	}
+
 	public async insertSnippets(rawText:string):Promise<string>{
 		const s = new MagicString(rawText);
 		// get rootpath
